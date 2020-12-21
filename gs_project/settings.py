69c04fbe869af9862c2d8507a1b13faac2afffed
ravellys <ravellyspyrrho@gmail.com>
2020-12-21 00:32:51 -0300

    update

--- gs_project/settings.py
+++ gs_project/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'gs_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #               'default': {
