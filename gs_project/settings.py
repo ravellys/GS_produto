15c9092960627212238608b1ae4df389d75cdf6b
ravellys <ravellyspyrrho@gmail.com>
2020-12-22 13:46:20 -0300

    videos

--- gs_project/settings.py
+++ gs_project/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
     'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
